2a3bcc5136e0d358c729f07e612ca15d54e4598c
a-tu-4-free <joho0678@gmail.com>
2026-04-30 11:27:01 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ fetch("memes.json")
   });
 
 function rand(a) {
+  if (!a || a.length === 0) return "（詞庫載入異常）";
   return a[Math.floor(Math.random() * a.length)];
 }
 
@@ -26,26 +27,40 @@ function id() {
 }
 
 function level() {
-  return rand(["LEVEL 1 可識別", "LEVEL 2 模糊", "LEVEL 3 匿名", "LEVEL 4 不存在"]);
+  return rand(["LEVEL 1 可識別", "LEVEL 2 模糊", "LEVEL 3 匿名", "LEVEL 4 不存在", "LEVEL 5 正在補正"]);
 }
 
 function time() {
   return new Date().toLocaleString('zh-TW');
 }
 
-// 改進版 generateMeme
+// 更黑毒版 generateMeme - 提高查帳與毒梗出現率
 function generateMeme() {
-  if (Math.random() < 0.07 && memes.rare) {
+  const isBlackMode = Math.random() < 0.45;   // 45% 機率進入黑毒模式
+
+  if (Math.random() < 0.08 && memes.rare) {
     return rand(memes.rare);
   }
 
-  return rand(memes.openings) + 
-         "<br><br>" + 
-         rand(memes.usages) + 
-         "<br><br>" + 
-         rand(memes.usages) + 
-         "<br><br>" + 
-         rand(memes.endings);
+  if (isBlackMode) {
+    // 黑毒模式：更容易出現查帳、17年、搞定司法等梗
+    return rand(memes.openings) +
+           "<br><br>" +
+           rand(memes.usages) +
+           "<br><br>" +
+           rand(memes.usages) +   // 第二句更容易是毒句
+           "<br><br>" +
+           rand(memes.endings);
+  } else {
+    // 一般模式
+    return rand(memes.openings) +
+           "<br><br>" +
+           rand(memes.usages) +
+           "<br><br>" +
+           rand(memes.usages) +
+           "<br><br>" +
+           rand(memes.endings);
+  }
 }
 
 function generate() {
@@ -55,8 +70,10 @@ function generate() {
   document.getElementById("rmoney").innerText = money();
   document.getElementById("rid").innerText = id();
   document.getElementById("rtime").innerText = time();
-  if (document.getElementById("rlevel")) {
-    document.getElementById("rlevel").innerText = level();
+
+  const rlevel = document.getElementById("rlevel");
+  if (rlevel) {
+    rlevel.innerText = level();
   }
 }
 
@@ -66,6 +83,25 @@ function spam() {
   let t = "";
   for (let i = 0; i < 8; i++) {
     t += generateMeme() + "<br><br><br>";
+  }
+  document.getElementById("result").innerHTML = t;
+}
+
+// 新增：專門的「查帳黑毒洗版」模式（可選）
+function spamBlack() {
+  if (!memes) return;
+
+  let t = "";
+  for (let i = 0; i < 7; i++) {
+    // 強制使用黑毒模式
+    const text = rand(memes.openings) +
+                 "<br><br>" +
+                 rand(memes.usages) +
+                 "<br><br>" +
+                 rand(memes.usages) +
+                 "<br><br>" +
+                 rand(memes.endings);
+    t += text + "<br><br><br>";
   }
   document.getElementById("result").innerHTML = t;
 }
